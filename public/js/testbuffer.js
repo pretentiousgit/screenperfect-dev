f5a1078a82302f69d6a7d754895cfc246048dc93
pretentiousgit <alex.leitch@gmail.com>
2013-08-29 16:37:37 -0400

    various synch testing elements added

--- public/js/testbuffer.js
+++ public/js/testbuffer.js
@@ -5,18 +5,23 @@ socket.on('connect', function () {
           $('#connection').trigger('disconnected');
           message('There is already a listener connected');
         });
+    document.getElementById('v0').play();
+});
+
+socket.on('timestamp', function(time) {
+    document.getElementById('v0').currentTime=time;
 });
 
 
 $(function (){
 var vid = $('#v0')[0];
 
-$('#v0').on('play', function() {
-    console.log('playing');
-    setInterval(function() { // needs to only do it while video is playing.
-        $('#time').html(vid.currentTime);
-        socket.emit('timestamp', vid.currentTime);
-    }, 100);
-});
+// $('#v0').on('play', function() {
+//     console.log('playing');
+//     setInterval(function() { // needs to only do it while video is playing.
+//         $('#time').html(vid.currentTime);
+//         socket.emit('timestamp', vid.currentTime);
+//     }, 100);
+// });
 
 });
