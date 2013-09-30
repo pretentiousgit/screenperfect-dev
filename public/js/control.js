2d0aae900feb6a6c1636a7f38d171365021c851d
pretentiousgit <alex.leitch@gmail.com>
2013-09-29 21:35:49 -0400

    boxes are generated responsively by percentage of canvas. Downside: canvas resizes with monitor. Fix: make canvas bg display fixed.

--- public/js/control.js
+++ public/js/control.js
@@ -57,18 +57,14 @@ $(function() {
 	var ts = $('#timestamp');
 
   	// hotspot reader
-	$.getJSON('/tmp/'+videoNext+'.json', function(data) {
-		 for (var i in data.spots) {
-		 	// these need to be dynamically recalculated by percentages.
+	$.getJSON('/tmp/1.json', function(data) {
+    for (var i in data.spots) {
 			var skeleton = $('<a class="send-video">&nbsp;</a>');
         	$(skeleton).attr('nextvid', data.spots[i].link);
-        	$(skeleton).attr('topSize', data.spots[i].top);
-        	$(skeleton).css('top', data.spots[i].top);
-        	$(skeleton).css('left', data.spots[i].left);
-        	$(skeleton).css('width',  (data.spots[i].width/720)*100+'%' ); //video-width should be adjustable
-        	$(skeleton).css('height', (data.spots[i].height/540)*100+'%'); //video-height should be adjustable
-        	$('#controlSpots').html(skeleton);
-        }
+        	$(skeleton).attr('style', data.spots[i].css);
+        	$("#controlSpots").html(skeleton);
+    }
+
 	 });
 
 	ts.css('color','#FFF');
